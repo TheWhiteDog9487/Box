7fe972f416568cd89cd3f5e84e4ec915a1a042de
TheWhiteDog9487 <60037547+TheWhiteDog9487@users.noreply.github.com>
2024-01-06 16:14:13 +0800

    Delete AzureSpeakerGUI directory
    迁移至https://github.com/TheWhiteDog9487/AzureSpeakerGUI

--- AzureSpeakerGUI/AzureSpeakerGUI/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "AzureSpeakerGUI.h"
-#include <QtWidgets/QApplication>
-
-int main(int argc, char *argv[]){
-    QApplication a(argc, argv);
-    AzureSpeakerGUI w;
-    w.show();
-    return a.exec();
-}
